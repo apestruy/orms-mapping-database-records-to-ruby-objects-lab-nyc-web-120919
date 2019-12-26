0e008548904a26271b5a5220894f1fa9ff669075
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-26 04:38:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,12 +25,17 @@ class Student
  
     			DB[:conn].execute(sql, name).map do |row|
       			     self.new_from_db(row)
-    			end.first
+    	end.first
   end
   
-  def 
-  
-  
+  def self.all_students_in_grade_9 
+    sql = <<-SQL
+            SELECT * FROM students
+            WHERE grade = "9"
+            SQL
+       	DB[:conn].execute(sql).map do |row|
+      			     self.new_from_db(row)
+    	end     
   end 
   
   
